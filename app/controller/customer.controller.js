2911a50baa3cc4e1ee915fc402d65b4b492ff458
funnyjerry <wenhu95piao@gmail.com>
2019-05-17 18:45:34 +0800

    excel-file-data-nodejs-express-restapi-download-extract-excel-xlsx-data-file-from-mysql

--- app/controller/customer.controller.js
+++ app/controller/customer.controller.js
@@ -1,4 +1,6 @@
 const db = require('../config/db.config.js');
+const excel = require('exceljs');
+
 const Customer = db.customers;
 
 // Post a Customer
@@ -46,5 +48,34 @@ exports.delete = (req, res) => {
 		where: { id: id }
 	}).then(() => {
 		res.status(200).send({ "success": true, "message": "deleted successfully a customer with id = " + id });
+	});
+};
+
+// FETCH all Customers
+exports.download = (req, res) => {
+	Customer.findAll().then(customers => {
+
+		const jsonCustomers = JSON.parse(JSON.stringify(customers));
+
+		let workbook = new excel.Workbook(); //creating workbook
+		let worksheet = workbook.addWorksheet('Customers'); //creating worksheet
+
+		worksheet.columns = [
+			{ header: 'Id', key: 'id', width: 10 },
+			{ header: 'First Name', key: 'firstname', width: 30 },
+			{ header: 'Last Name', key: 'lastname', width: 30 },
+			{ header: 'Age', key: 'age', width: 10, outlineLevel: 1 }
+		];
+
+		// Add Array Rows
+		worksheet.addRows(jsonCustomers);
+
+		res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+		res.setHeader('Content-Disposition', 'attachment; filename=' + 'customer.xlsx');
+
+		return workbook.xlsx.write(res)
+			.then(function () {
+				res.status(200).end();
+			});
 	});
 };
--- app/route/customer.route.js
+++ app/route/customer.route.js
@@ -2,18 +2,12 @@ module.exports = function(app) {
  
     const customers = require('../controller/customer.controller.js');
  
-    // Create a new Customer
+    //Customer
     app.post('/api/addCustomer', customers.create);
- 
-    // Retrieve all Customer
     app.get('/api/getAllCustomers', customers.findAll);
- 
-    // Retrieve a single Customer by Id
     app.get('/api/getOneCustomer/:customerId', customers.findById);
- 
-    // Update a Customer with Id
     app.put('/api/updateCustomer/:customerId', customers.update);
- 
-    // Delete a Customer with Id
     app.delete('/api/deleteCustomer/:customerId', customers.delete);
+    app.get('/api/downloadAllCustomers/excel', customers.download);
+
 }
