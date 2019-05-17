1d4de730cc15d540e5484f8b3825cd36826eaa50
funnyjerry <wenhu95piao@gmail.com>
2019-05-18 02:42:42 +0800

    nodejs-express-bootstrap-image

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+
 var bodyParser = require('body-parser');
 const db = require('./app/config/db.config.js');
 
 app.use(bodyParser.json())
+
 var router = express.Router();
 var path = __dirname + '/views/';
 
@@ -13,29 +15,21 @@ var path = __dirname + '/views/';
 // });
 
 require('./app/route/customer.route.js')(app);
- 
-router.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
-});
- 
-router.get("/",function(req,res){
-  res.sendFile(path + "index.html");
-});
- 
-router.get("/about",function(req,res){
-  res.sendFile(path + "about.html");
+
+router.use(function (req,res,next) { console.log("/" + req.method); next();});
+
+
+////////////
+// Router //
+////////////
+router.get("/",function(req,res){ res.sendFile(path + "index.html")});
+router.get("/about",function(req,res){ res.sendFile(path + "about.html")});
+app.get("/api/image", function(req,res){
+  return res.sendFile(__dirname + "/resources/static/image/astro.png");
 });
 
 app.use("/", router);
- 
-app.use("*",function(req,res){
-  res.sendFile(path + "404.html");
-});
-
-
-
-
+app.use("*",function(req,res){ res.sendFile(path + "404.html")});
 
 // Create a Server
 var server = app.listen(8081, function () {
