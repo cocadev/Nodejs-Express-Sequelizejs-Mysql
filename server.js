1586fde1c88da87fb8128097086f21c8ca36e210
funnyjerry <wenhu95piao@gmail.com>
2019-05-18 00:26:26 +0800

    Express Bootstrap views

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-app.use(bodyParser.json())
-
 const db = require('./app/config/db.config.js');
-  
+
+app.use(bodyParser.json())
+var router = express.Router();
+var path = __dirname + '/views/';
+
 // force: true will drop the table if it already exists
 // db.sequelize.sync({force: true}).then(() => {
 //   console.log('Drop and Resync with { force: true }');
@@ -12,6 +14,29 @@ const db = require('./app/config/db.config.js');
 
 require('./app/route/customer.route.js')(app);
  
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+ 
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+ 
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
+});
+
+app.use("/", router);
+ 
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+
+
+
+
 // Create a Server
 var server = app.listen(8081, function () {
  
